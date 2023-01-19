86aede819639c31cad460684c38e40e81033535f
Vaibhav Gaur <61357479+vaibhav-gaur-518@users.noreply.github.com>
2023-01-19 21:11:02 +0530

    Add files via upload

---
+++ Reflection.java
@@ -0,0 +1,38 @@
+package com.monocept.test;
+
+import java.lang.reflect.Method;
+
+public class Reflection {
+	public static void main(String[] args) {
+		try {
+			Class c = Class.forName("java.lang." + args[0]);
+			Method methlist[] = c.getDeclaredMethods();
+			for (int i = 0; i < methlist.length; i++) {
+				Method m = methlist[i];
+				System.out.println("name  = " + m.getName());
+				Class pvec[] = m.getParameterTypes();
+				for (int j = 0; j < pvec.length; j++) {
+					System.out.println("param #" + j + " " + pvec[j]);
+					System.out.println("return type = " + m.getReturnType());
+				}
+				System.out.println("");
+			}
+			System.out.println("-----------------------------------");
+
+			Class c1 = Class.forName("java.lang." + args[1]);
+			Method methlist2[] = c1.getDeclaredMethods();
+			for (int i = 0; i < methlist2.length; i++) {
+				Method m = methlist2[i];
+				System.out.println("name  = " + m.getName());
+				Class pvec2[] = m.getParameterTypes();
+				for (int j = 0; j < pvec2.length; j++) {
+					System.out.println("param #" + j + " " + pvec2[j]);
+					System.out.println("return type = " + m.getReturnType());
+				}
+				System.out.println("");
+			}
+		} catch (Throwable e) {
+			System.err.println(e);
+		}
+	}
+}
